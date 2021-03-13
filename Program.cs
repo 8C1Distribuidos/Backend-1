9c83a55db88db03b28434be373add647694e1afc
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-03-13 14:43:20 -0600

    Prueba
    Solo para ir incluyendo algo donde empezar a trabajar, honestamente no estoy muy seguro si los commit funcionan de la misma manera con los repositorios privados...
    Mmmm me dice que no tengo derechos de escritura en Backend-1, probablemente se los debo pedir a patron

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+//using System.Microsoft.EntityFrameWorkCore;
+
+namespace Backend_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("JAJAJA");
+            WriteLine("Intentando recordar como programar XD");
+            WriteLine("Para Trbajar con EntityFrameWork se necesitaba editar algo en el csproj");
+            WriteLine("Nota: Revisar el video de LogicaDifusa Coneccion DB 7C");
+
+        }
+    }
+}
