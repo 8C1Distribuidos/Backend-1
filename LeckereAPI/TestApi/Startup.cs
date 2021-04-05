21c227d72875243d1e58f8c0a4f17cccb84bc622
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-04-05 11:33:16 -0500

    rrr
    cosas que le habia movido

--- LeckereAPI/TestApi/Startup.cs
+++ LeckereAPI/TestApi/Startup.cs
@@ -42,7 +42,7 @@ namespace TestApi
 
             app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
