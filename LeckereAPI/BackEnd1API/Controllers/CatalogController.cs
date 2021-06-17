d663883455d7c0e75c6b11f0638087925e92e279
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-06-17 09:02:36 -0500

    cosas

--- LeckereAPI/BackEnd1API/Controllers/CatalogController.cs
+++ LeckereAPI/BackEnd1API/Controllers/CatalogController.cs
@@ -107,57 +107,5 @@ namespace BackEnd1API.Controllers
                 return NotFound(ex.Message);
             }
         }
-
-        /*
-        [HttpPost("Post")]
-        public bool PostCatalog(Catalog newCatalog){
-            //Catalog c = JsonHandler<Catalog>.Deserialize(jsonString);
-            foreach (Catalog c in fake.catalogs)
-            {
-                if(c.id == newCatalog.id){
-                    return false;
-                }
-            }
-            fake.catalogs.Add(newCatalog);
-            return true;
-        }
-        [HttpPost("PostJson")]
-        public bool PostCatalogJson(string jsonString ){
-            Catalog newCatalog = JsonHandler<Catalog>.Deserialize(jsonString);
-            if(newCatalog == null)return false;
-            foreach (Catalog c in fake.catalogs)
-            {
-                if(c.id == newCatalog.id){
-                    return false;
-                }
-            }
-            fake.catalogs.Add(newCatalog);
-            return true;
-        }
-
-        
-        [HttpPut("Put")]
-        public bool PutCatalog(Catalog updatedCatalog){
-            //Catalog updatedCatalog = JsonHandler<Catalog>.Deserialize(jsonString);
-            foreach(Catalog c in fake.catalogs){
-                if(c.id == updatedCatalog.id){
-                    c.name = updatedCatalog.name;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        [HttpDelete("Delete")]
-        public bool DeleteCatalog(int id){
-            foreach(Catalog c in fake.catalogs){
-                if(c.id == id){
-                    fake.catalogs.Remove(c);
-                    return true;
-                }
-            }
-            return false;
-        }
-        */
     }
 }
